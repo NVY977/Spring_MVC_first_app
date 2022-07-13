1cf63b96d367a070058ce2443f7e3440dbc27f25
NVY977 <nvy977@gmail.com>
2022-07-13 22:13:20 +0300

    First part JDBC

--- src/main/java/ru/nvy/springmvc/dao/person/PersonDAO.java
+++ src/main/java/ru/nvy/springmvc/dao/person/PersonDAO.java
@@ -2,25 +2,61 @@ package ru.nvy.springmvc.dao.person;
 
 import org.springframework.stereotype.Component;
 import ru.nvy.springmvc.model.person.Person;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-
     private static int PEOPLE_COUNT;
-    private List<Person> people;
-    {
-        //блок инициализации
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 35, "Ard", "test@email.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Mark", 51, "Wahlberg", "test1@email.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Nick", 20, "Yazykov", "test2@email.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Leon", 21, "Kennedy", "test3@email.com"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "1590";
+    private static Connection connection;
+
+    static {
+        //проверка на допустимость использования postgresql.Driver
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        //устанавливаем соединение с БД
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
     }
-    public List<Person> index(){
+
+    /**
+     * Show all people
+     * @return
+     */
+    public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
+
+
+
 
     /**
      * Show only one Person
@@ -30,16 +66,24 @@ public class PersonDAO {
     public Person show(int id){
         //лямбда выражение
         //найти человека по id если не нашли вернуть null
-        return people.stream().filter(items->items.getId() == id).findAny().orElse(null);
+ //       return people.stream().filter(items->items.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     /**
      * Add new person
-     * @param item
+     * @param person
      */
-    public void save(Person item){
-        item.setId(++PEOPLE_COUNT); // устанавливаем Id, ибо человек не должен сам вводить id
-        people.add(item);
+    public void save(Person person){
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     /**
@@ -48,11 +92,11 @@ public class PersonDAO {
      * @param updatePerson
      */
     public void update(int id, Person updatePerson){
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setSurname(updatePerson.getSurname());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatePerson.getName());
+//        personToBeUpdated.setAge(updatePerson.getAge());
+//        personToBeUpdated.setSurname(updatePerson.getSurname());
+//        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     /**
@@ -60,7 +104,7 @@ public class PersonDAO {
      * @param id
      */
     public void delete(int id){
-        people.removeIf(item->item.getId() == id);
+//        people.removeIf(item->item.getId() == id);
     }
 
 }

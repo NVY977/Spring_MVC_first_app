6f6815ea2fe27e175fa9c9a026cf6170b58ff5c5
NVY977 <nvy977@gmail.com>
2022-07-13 14:36:44 +0300

    Adding Form Validation

--- src/main/java/ru/nvy/springmvc/controllers/people/PeopleController.java
+++ src/main/java/ru/nvy/springmvc/controllers/people/PeopleController.java
@@ -1,11 +1,15 @@
 package ru.nvy.springmvc.controllers.people;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.nvy.springmvc.dao.person.PersonDAO;
 import ru.nvy.springmvc.model.person.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -13,7 +17,7 @@ public class PeopleController {
     private final PersonDAO personDAO;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO){
+    public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
@@ -32,31 +36,40 @@ public class PeopleController {
     }
 
     @GetMapping("/new")
-    public String newPerson(Model model){
+    public String newPerson(Model model) {
         model.addAttribute("person", new Person());
         return "people/new";
     }
 
+
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
 
     @GetMapping("/{id}/edit")
-    public String edit(@PathVariable("id") int id, Model model){
+    public String edit(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
 
     @PatchMapping("/{id}")
-    public String Update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String Update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id){
+    public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
         return "redirect:/people";
     }
--- src/main/java/ru/nvy/springmvc/model/person/Person.java
+++ src/main/java/ru/nvy/springmvc/model/person/Person.java
@@ -1,11 +1,24 @@
 package ru.nvy.springmvc.model.person;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     //region Fields
     private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 20, message = "Name should not be between 2 and 20 characters")
     private String name;
+    @Min(value = 10, message = "Age should be greater than 10")
     private int age;
+
+    @NotEmpty(message = "Surname should not be empty")
+    @Size(min = 2, max = 20, message = "Surname should not be between 2 and 20 characters")
     private String surname;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
     //endregion
 
